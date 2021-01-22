d827fe5e2410fc375edf4881d1ae71b1e2810b0e
Steph St <steph.streete@outlook.com>
2021-01-22 21:27:28 +0000

    added class

--- main.rb
+++ main.rb
@@ -1,9 +1,14 @@
 class Cars
   def make
-    puts "Your car make is Audi"
+    puts "Your car make is Audi!"
   end
 
   def model
     puts "Your model is S3!"
   end
 end
+
+Cars = Cars.new
+
+puts Cars.make
+puts Cars.model
